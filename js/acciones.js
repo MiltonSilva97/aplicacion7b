4301ee8d6f32fcc2eb03be9d4b1a5237b8a8ffd2
MiltonSilva97 <milton19994@gmail.com>
2016-05-02 18:22:31 -0500

    subida3
    aplicacion7d

--- js/acciones.js
+++ js/acciones.js
@@ -1,9 +1,8 @@
-// JavaScript Document
-
-$(document).ready(function (e){
- document .addEventListener ("deviceready",function(){
- $('#btnvibrar').on('tap',function(){
-	 navigator.notification.vibrate(10000);
- });
- });
+//javaScrip Document
+$(document).ready(function(e) {
+document.addEvenListener( 'deviceready',function(){
+	$('#btnvibrar').on('tap',function(){
+		navigator.notification.vibrate(2000);
+    });
+  });	
 });
